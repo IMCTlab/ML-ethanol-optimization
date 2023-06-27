2271c47a88f1400e40a1af75d7daea2c8f5224aa
KoHaku55 <35957069+KoHaku55@users.noreply.github.com>
2023-06-27 15:11:04 +0700

    Update Visualization_ethanol_30C.R

--- Scripts/Visualization_ethanol_30C.R
+++ Scripts/Visualization_ethanol_30C.R
@@ -3,12 +3,12 @@ library(ggplot2)
 library(reshape2)
 library(readxl)
 library(ggpubr)
-library(ggsci)
+
 ##################
 
 #Visualization 
 #rep1 30 C
-setwd("D:/BIOTEC NSTDA/ML_ethanol/Results/30C/raw/")
+
 dat=read_xlsx("Metabolite and growth data_Day 1 and 2.xlsx", sheet = 1)
 #define column name
 colnames(dat)=c("Name","A600_24h","A600_24h_std","A600_48h","A600_48h_std",
@@ -28,80 +28,92 @@ dat1_24=dat1[,grepl("24h",colnames(dat1))]
 dat1_24$Name=row.names(dat1_24)
 dat1_24$`EtOH_24h(g/L/OD600)`= dat1_24$EtOH_24h/dat1_24$A600_24h
 
-X=dat1_24[dat1_24$EtOH_24h>37.83359,] #xxx
-X2=X[order(X$EtOH_24h, decreasing = T),]
-log2(61.96128/37.83359)
-log2(52.86604/37.83359)
-####  plot1 #
-P1=ggpairs(dat1_24[,-7])  #or ggpairs(dat1_24, columns=1:6)
+
 
 ####  plot2 #EtOH vs other 
-p1=ggplot(dat1_24, aes(`EtOH_24h(g/L/OD600)`, A600_24h, color=Name, label=Name))+
+c=cor.test(dat1_24$EtOH_24h,dat1_24$A600_24h)
+p1=ggplot(dat1_24, aes(EtOH_24h, A600_24h, color=Name, label=Name))+
   geom_point(size=2.5, alpha=0.7)+
+  annotate(geom="text", size=6,x=16,y=32,label=paste("Pearson's correlation=",round(c$estimate,2)))+
   #ggtitle("30C EtOH production vs OD600")+
-  xlab("Specific product of ethanol (g/L/OD600)")+ylab("OD600")+
-  xlim(0,round(max(dat1_24$`EtOH_24h(g/L/OD600)`)+0.5))+ylim(0,round(max(dat1_24$A600_24h)))+
+  xlab("Ethanol production (g/L)")+ylab(bquote(OD[600]))+
+  xlim(0,round(max(dat1_24$EtOH_24h)+0.5))+ylim(0,round(max(dat1_24$A600_24h)+0.5))+
   #geom_text(size=2,hjust =0.5, vjust=1.5, color="grey")
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5),
-        legend.position="none")
-#p1+scale_color_jco()
-p2=ggplot(dat1_24, aes(`EtOH_24h(g/L/OD600)`, Glu_24h, color=Name, label=Name))+
+        legend.position="none",
+        text = element_text(size=20))
+
+c=cor.test(dat1_24$EtOH_24h,dat1_24$Glu_24h)
+p2=ggplot(dat1_24, aes(EtOH_24h, Glu_24h, color=Name, label=Name))+
   geom_point(size=2.5, alpha=0.7 )+
+  annotate(geom="text",size=6,x=16,y=96,label=paste("Pearson's correlation=",round(c$estimate,2)))+
   #ggtitle("30C EtOH production vs %Glu consumption")+
-  xlab("Specific product of ethanol (g/L/OD600)")+ylab("% Glucose comsumption")+
-  xlim(0,round(max(dat1_24$`EtOH_24h(g/L/OD600)`)+0.5))+ylim(0,round(max(dat1_24$Glu_24h)))+
+  xlab("Ethanol production (g/L)")+ylab("% Glucose comsumption")+
+  xlim(0,round(max(dat1_24$EtOH_24h)+0.5))+ylim(0,round(max(dat1_24$Glu_24h)+0.5))+
   #geom_text(size=2,hjust =0.5, vjust=1.5, color="grey")
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5),
-        legend.position="none")
+        legend.position="none",
+        text = element_text(size=20))
 
-p3=ggplot(dat1_24, aes(`EtOH_24h(g/L/OD600)`, EtOH_24h, color=Name, label=Name))+
+c=cor.test(dat1_24$EtOH_24h,dat1_24$`EtOH_24h(g/L/OD600)`)
+p3=ggplot(dat1_24, aes(EtOH_24h, `EtOH_24h(g/L/OD600)`, color=Name, label=Name))+
   geom_point(size=2.5, alpha=0.7)+
+  annotate(geom="text", size=6,x=16,y=2.8,label=paste("Pearson's correlation=",round(c$estimate,2)))+
   #ggtitle("30C EtOH (g/L/OD600) vs EthOH (g/L)")+
-  xlab("Specific product of ethanol (g/L/OD600)")+ylab("Ethanol production (g/L)")+
-  xlim(0,round(max(dat1_24$`EtOH_24h(g/L/OD600)`)+0.5))+ylim(0,round(max(dat1_24$EtOH_24h)))+
+  xlab("Ethanol production (g/L)")+
+  ylab(expression(paste("Specific product of ethanol (g/L/",OD[600],")")))+
+  xlim(0,round(max(dat1_24$EtOH_24h)+0.5))+ylim(0,round(max(dat1_24$`EtOH_24h(g/L/OD600)`)+0.5))+
   #geom_text(size=2,hjust =0.5, vjust=1.5, color="grey")
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5),
-        legend.position="none")
+        legend.position="none",
+        text = element_text(size=20))
 
-p4=ggplot(dat1_24, aes(`EtOH_24h(g/L/OD600)`, Gly_24h, color=Name, label=Name))+
+c=cor.test(dat1_24$EtOH_24h,dat1_24$Gly_24h)
+p4=ggplot(dat1_24, aes(EtOH_24h, Gly_24h, color=Name, label=Name))+
   geom_point(size=2.5, alpha=0.7)+
+  annotate(geom="text", size=6,x=16,y=4.6,label=paste("Pearson's correlation=",round(c$estimate,2)))+
   #ggtitle("30C EtOH production vs Glycerol")+
-  xlab("Specific product of ethanol (g/L/OD600)")+ylab("Glycerol (g/L)")+
-  xlim(0,round(max(dat1_24$`EtOH_24h(g/L/OD600)`)+0.5))+ylim(0,round(max(dat1_24$Gly_24h)))+
+  xlab("Ethanol production (g/L)")+ylab("Glycerol (g/L)")+
+  xlim(0,round(max(dat1_24$EtOH_24h)+0.5))+ylim(0,round(max(dat1_24$Gly_24h)+0.5))+
   #geom_text(size=2,hjust =0.5, vjust=1.5, color="grey")
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5),
-        legend.position="none")
+        legend.position="none",
+        text = element_text(size=20))
 
-p5=ggplot(dat1_24, aes(`EtOH_24h(g/L/OD600)`, Pyruvate_24h, color=Name, label=Name))+
+c=cor.test(dat1_24$EtOH_24h,dat1_24$Pyruvate_24h)
+p5=ggplot(dat1_24, aes(EtOH_24h, Pyruvate_24h, color=Name, label=Name))+
   geom_point(size=2.5, alpha=0.7)+
+  annotate(geom="text", size=6,x=16,y=1.85,label=paste("Pearson's correlation=",round(c$estimate,2)))+
   #ggtitle("30C EtOH production vs Pyruvate")+
-  xlab("Specific product of ethanol (g/L/OD600)")+ylab("Pyruvate (g/L)")+
-  xlim(0,round(max(dat1_24$`EtOH_24h(g/L/OD600)`)+0.5))+ylim(0,round(max(dat1_24$Pyruvate)))+
+  xlab("Ethanol production (g/L)")+ylab("Pyruvate (g/L)")+
+  xlim(0,round(max(dat1_24$EtOH_24h)+0.5))+ylim(0,round(max(dat1_24$Pyruvate)+0.5))+
   #geom_text(size=2,hjust =0.5, vjust=1.5, color="grey")
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5),
-        legend.position="none")
+        legend.position="none",
+        text = element_text(size=20))
 
-p6=ggplot(dat1_24, aes(`EtOH_24h(g/L/OD600)`, Acetate_24h, color=Name, label=Name))+
+c=cor.test(dat1_24$EtOH_24h,dat1_24$Acetate_24h)
+p6=ggplot(dat1_24, aes(EtOH_24h, Acetate_24h, color=Name, label=Name))+
   geom_point(size=2.5, alpha=0.7)+
+  annotate(geom="text", size=6,x=16,y=2.8,label=paste("Pearson's correlation=",round(c$estimate,2)))+
   #ggtitle("30C EtOH production vs Acetate")+
-  xlab("Specific product of ethanol (g/L/OD600)")+ylab("Acetate (g/L)")+
-  xlim(0,round(max(dat1_24$`EtOH_24h(g/L/OD600)`)+0.5))+ylim(0,round(max(dat1_24$Acetate_24h)))+
+  xlab("Ethanol production (g/L)")+ylab("Acetate (g/L)")+
+  xlim(0,round(max(dat1_24$EtOH_24h)+0.5))+ylim(0,round(max(dat1_24$Acetate_24h)+0.5))+
   #geom_text(size=2,hjust =0.5, vjust=1.5, color="grey")
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5),
-        legend.position="none")
+        legend.position="none",
+        text = element_text(size=20))
 
 
 p_eth40C=ggarrange(p3,p1,p2,p4,p5,p6,ncol=3, nrow = 2)
-P2=annotate_figure(p_eth40C, top = text_grob("production at 30 C, 24h ", 
-                                          color = "black", face = "bold", size = 14))
-                                          
-                                          
+P2=annotate_figure(p_eth40C)
+                          
 # Separate plots
 n=as.data.frame(dat1_24[order(dat1_24$EtOH_24h, decreasing = T),"Name"])
 Xn=as.data.frame(dat1_24[order(dat1_24$EtOH_24h, decreasing = T),])
@@ -114,7 +126,8 @@ w1=ggplot(dat1_24_2, aes(x=Name,y=Pyruvate_24h, group=1))+
   ggtitle("Pyruvate")+theme_classic2()+
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_blank(),
-        axis.ticks.x= element_blank())+
+        axis.ticks.x= element_blank(),
+        text = element_text(size=20))+
   geom_hline(yintercept = dat1_24_2[dat1_24_2$Name%in%"xxx","Pyruvate_24h"],
               linetype="dashed")
 
@@ -125,7 +138,8 @@ w2=ggplot(dat1_24_2, aes(x=Name,y=Acetate_24h, group=1))+
   ggtitle("Acetate")+theme_classic2()+
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_blank(),
-        axis.ticks.x= element_blank())+
+        axis.ticks.x= element_blank(),
+        text = element_text(size=20))+
   geom_hline(yintercept =dat1_24_2[dat1_24_2$Name%in%"xxx","Acetate_24h"],
               linetype="dashed")
 
@@ -136,7 +150,8 @@ w3=ggplot(dat1_24_2, aes(x=Name,y=Gly_24h, group=1))+
   ggtitle("Glycerol")+theme_classic2()+
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_blank(),
-        axis.ticks.x= element_blank())+
+        axis.ticks.x= element_blank(),
+        text = element_text(size=20))+
   geom_hline(yintercept = dat1_24_2[dat1_24_2$Name%in%"xxx","Gly_24h"],
               linetype="dashed")
 
@@ -144,10 +159,11 @@ w4=ggplot(dat1_24_2, aes(x=Name,y=`EtOH_24h(g/L/OD600)`, group=1))+
   geom_line(size=1)+ #, color="#C77CFF"
   xlab("strains")+ylab(expression(paste("ethanol (g/L/",OD[600],")")))+
   scale_y_continuous(breaks = seq(0,3,0.5), limits = c(0,3))+
-  ggtitle("Specific product of ethanol)")+theme_classic2()+
+  ggtitle("Specific product of ethanol")+theme_classic2()+
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_blank(),
-        axis.ticks.x= element_blank())+
+        axis.ticks.x= element_blank(),
+        text = element_text(size=20))+
   geom_hline(yintercept =  dat1_24_2[dat1_24_2$Name%in%"xxx","EtOH_24h(g/L/OD600)"], 
               linetype="dashed")
 
@@ -159,7 +175,8 @@ w5=ggplot(dat1_24_2, aes(x=Name,y=EtOH_24h, group=1))+
   ggtitle("Ethanol")+theme_classic2()+
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_blank(),
-        axis.ticks.x= element_blank())+
+        axis.ticks.x= element_blank(),
+        text = element_text(size=20))+
   geom_hline(yintercept =  dat1_24_2[dat1_24_2$Name%in%"xxx","EtOH_24h"], 
              linetype="dashed")
 
@@ -170,7 +187,8 @@ w6=ggplot(dat1_24_2, aes(x=Name,y=Glu_24h, group=1))+
   ggtitle("%Glucose consumption")+theme_classic2()+
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_blank(),
-        axis.ticks.x= element_blank())+
+        axis.ticks.x= element_blank(),
+        text = element_text(size=20))+
   geom_hline(yintercept =  dat1_24_2[dat1_24_2$Name%in%"xxx","Glu_24h"],
               linetype="dashed")
 
@@ -181,11 +199,9 @@ w7=ggplot(dat1_24_2, aes(x=Name,y=A600_24h, group=1))+
   ggtitle(bquote(OD[600]))+theme_classic2()+
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_blank(),
-        axis.ticks.x= element_blank())+
+        axis.ticks.x= element_blank(),
+        text = element_text(size=20))+
   geom_hline(yintercept = dat1_24_2[dat1_24_2$Name%in%"xxx","A600_24h"], 
               linetype="dashed")
 
 ww=ggarrange(w5,w4,w6,w7,w1,w2,w3,ncol=3, nrow = 3)
-ww
-
-
